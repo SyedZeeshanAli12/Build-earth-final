0a43347852d9df8c34fe2b0eb5df7f884f41bc9c
SyedZeeshanAli12 <syedzeeshanali877@gmail.com>
2021-05-04 15:52:54 +0500

    build-earth-finale

---
+++ models/Property.js
@@ -0,0 +1,42 @@
+const mongoose = require("mongoose"); 
+
+
+const Property =  mongoose.model("Property", new mongoose.Schema({
+    
+    society:{
+        type:String,
+        required:true,
+        minlength:5,
+        maxlength:50
+    },
+    name: {
+      type: String,
+      minlength: 5,
+      maxlength: 50,
+    },
+
+    category:{
+      type: String,
+      minlength:5,
+      maxlength: 50
+    },
+    minprice:{
+        type:Number,
+        min:0,
+    }, 
+    maxprice:{
+      type:Number,
+      max:70000000
+    },
+    image:{
+      type: String
+    },
+    createdAt:{
+      type: Date,
+      default:Date.now,
+      expires: {$gte:'1m', $lte:'3m'},
+    },
+    
+    }
+));
+module.exports = Property
